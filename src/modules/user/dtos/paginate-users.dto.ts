3876bb87b592a78068d19b8bf0f11f17d3806a36
Guxxtavoww <gustavoaugustocar@hotmail.com>
2024-09-02 13:00:24 -0300

    Chore: first commit

--- src/modules/user/dtos/paginate-users.dto.ts
+++ src/modules/user/dtos/paginate-users.dto.ts
@@ -5,12 +5,8 @@ import { ApiPropertyOptional } from '@nestjs/swagger';
 import { optionalStringSchemaToLowerCase } from 'src/shared/schemas.shared';
 import { createPaginationSchema } from 'src/utils/create-pagination-schema.utils';
 
-import { UserRole } from '../enums/user-role.enum';
-import { optionalUserRoleSchema } from '../schemas/user-role.schema';
-
 export const paginateUsersSchema = createPaginationSchema({
   user_name: optionalStringSchemaToLowerCase,
-  user_role: optionalUserRoleSchema,
 });
 
 export type PaginateUsersPayload = z.infer<typeof paginateUsersSchema>;
@@ -18,12 +14,4 @@ export type PaginateUsersPayload = z.infer<typeof paginateUsersSchema>;
 export class PaginateUsersDTO extends createZodDto(paginateUsersSchema) {
   @ApiPropertyOptional({ type: String, description: 'Optional user name' })
   user_name?: string;
-
-  @ApiPropertyOptional({
-    type: 'enum',
-    description: 'Optional User Role',
-    enum: UserRole,
-    example: UserRole.TENANT,
-  })
-  user_role?: UserRole;
 }
