4da3e2dc52b34c2b3f4d0cbb7157ffc8ba0da300
Guxxtavoww <gustavoaugustocar@hotmail.com>
2024-09-11 18:08:12 -0300

    Chore: adding health module

--- src/modules/concierge.module.ts
+++ src/modules/concierge.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { HealthModule } from './health/health.module';
 import { ScheduleModule } from './schedule/schedule.module';
 import { CondominiumModule } from './condominium/condominium.module';
 import { CondominiumMemberModule } from './condominium-member/condominium-member.module';
@@ -13,6 +14,7 @@ import { CondominiumMemberModule } from './condominium-member/condominium-member
     CondominiumModule,
     CondominiumMemberModule,
     ScheduleModule,
+    HealthModule
   ],
 })
 export class ConciergeModule {}
