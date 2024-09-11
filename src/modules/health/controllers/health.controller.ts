17fc1527b7d78734587cd56fb82e591c94ea2bec
Guxxtavoww <gustavoaugustocar@hotmail.com>
2024-09-11 18:10:00 -0300

    Chore: adding health module

--- src/modules/health/controllers/health.controller.ts
+++ src/modules/health/controllers/health.controller.ts
@@ -2,6 +2,7 @@ import {
   HealthCheck,
   HealthCheckService,
   MemoryHealthIndicator,
+  type HealthCheckResult,
 } from '@nestjs/terminus';
 import { ApiTags } from '@nestjs/swagger';
 import { Controller, Get } from '@nestjs/common';
@@ -19,7 +20,7 @@ export class HealthController {
   @Public()
   @Get('/')
   @HealthCheck()
-  check() {
+  async check(): Promise<HealthCheckResult> {
     return this.health.check([
       async () => this.memory.checkHeap('memory_heap', 200 * 1024 * 1024),
       async () => this.memory.checkRSS('memory_rss', 3000 * 1024 * 1024),
