d485ca74b87ee0a1ba76bf84d4ee22b90008f7c7
Gustavo Augusto <81970233+Guxxtavoww@users.noreply.github.com>
2024-09-23 15:58:55 -0300

    Chore: adding background jobs for cron jobs persistance

--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -3,7 +3,7 @@ import type { JwtSignOptions, JwtVerifyOptions } from '@nestjs/jwt';
 import { ENV_VARIABLES } from './env.config';
 
 export const jwtConstants = {
-  secret: ENV_VARIABLES.JWT_EXPIRES_IN,
+  secret: ENV_VARIABLES.JWT_SECRET,
   expiresIn: ENV_VARIABLES.JWT_EXPIRES_IN,
   audience: ENV_VARIABLES.JWT_AUDIENCE,
   issuer: ENV_VARIABLES.JWT_ISSUER,
